ff475b1e7e28e217130d8f8776872ff10ebf1030
jlisi-thrive <96087149+jlisi-thrive@users.noreply.github.com>
2023-04-19 16:49:09 -0400

    Update torntest.py

--- _runners/torntest.py
+++ _runners/torntest.py
@@ -1,13 +1,5 @@
-from salt.utils.asynchronous import current_ioloop
-import salt.ext.tornado.ioloop
+import salt.ext.tornado.gen
 
 def torn():
     orig_loop = salt.ext.tornado.ioloop.IOLoop.current()
-    return orig_loop
-
-# import salt.ext.tornado.gen
-# from salt.ext.tornado.concurrent import Future
-         
-# @salt.ext.tornado.gen.coroutine
-# def torn(self):
-#     return "HEY"
+    return "Jey"
