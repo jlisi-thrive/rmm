28fa50011b5a63a264bbbcf4d1f0444c9b665318
jlisi-thrive <96087149+jlisi-thrive@users.noreply.github.com>
2023-04-19 09:54:57 -0400

    Update test.py

--- _runners/test.py
+++ _runners/test.py
@@ -1,2 +1,15 @@
+from azure.eventgrid import EventGridPublisherClient,EventGridEvent
+from azure.core.credentials import AzureKeyCredential
+
 def testing():
-  return __opts__.get("mongo.uri", "Not Set")
+  var topicKey = __opts__.get("grid.key", "Not Set")
+  var topicEndpoint = __opts__.get("grid.endpoint", "Not Set")
+  credential = AzureKeyCredential(topic_key)
+  client = EventGridPublisherClient(endpoint, credential)
+  event = EventGridEvent(
+      data={"team": "azure-sdk"},
+      subject="presence/minionfromrunner",
+      event_type="minion_presence",
+      data_version="1.0"
+  )
+  return client.send(event)
