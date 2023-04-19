f6b8b3ba6030832e7e7cb90d12c61240564535c5
jlisi-thrive <96087149+jlisi-thrive@users.noreply.github.com>
2023-04-19 16:27:20 -0400

    Update torntest.py

--- _runners/torntest.py
+++ _runners/torntest.py
@@ -2,8 +2,5 @@ import salt.ext.tornado.gen
 from salt.ext.tornado.concurrent import Future
          
 @salt.ext.tornado.gen.coroutine
-def torn():
-    future = Future()
-    http_client = AsyncHTTPClient()
-    response = yield http_client.fetch("https://jsonplaceholder.typicode.com/posts/1")
-    raise salt.ext.tornado.gen.Return(response)
+def torn(self):
+    return "HEY"
