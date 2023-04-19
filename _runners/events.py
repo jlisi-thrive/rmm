6f469fc5447af07e0bd6eb23401ba9be99ecd84b
jlisi-thrive <96087149+jlisi-thrive@users.noreply.github.com>
2023-04-19 12:42:31 -0400

    Update events.py

--- _runners/events.py
+++ _runners/events.py
@@ -1,6 +1,25 @@
 import json
+import asyncio
+from azure.servicebus.aio import ServiceBusClient
+from azure.servicebus import ServiceBusMessage
 from azure.eventgrid import EventGridPublisherClient,EventGridEvent
 from azure.core.credentials import AzureKeyCredential
+
+async def send_single_message(sender):
+    # Create a Service Bus message and send it to the queue
+    message = ServiceBusMessage(body="Test", subject="TestSubject")
+    await sender.send_messages(message)
+    print("Sent a single message")
+              
+async def sendTopic(data):
+  topicConnection = __opts__.get("topic.string", "Not Set")
+  topicName = __opts__.get("topic.name", "Not Set")
+  async with ServiceBusClient.from_connection_string(
+          conn_str=topicConnection,
+          logging_enable=True) as servicebus_client:
+          sender = servicebus_client.get_topic_sender(topic_name=topicName)
+          async with sender:
+              await send_single_message(sender)
 
 def jobs(data):
   topicKey = __opts__.get("grid.key", "Not Set")
