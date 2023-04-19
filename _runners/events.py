ff6ef5f39d31d1df34384ecacc63cc726eb70853
jlisi-thrive <96087149+jlisi-thrive@users.noreply.github.com>
2023-04-19 10:07:37 -0400

    Update events.py

--- _runners/events.py
+++ _runners/events.py
@@ -1,13 +1,13 @@
 from azure.eventgrid import EventGridPublisherClient,EventGridEvent
 from azure.core.credentials import AzureKeyCredential
 
-def jobs():
+def jobs(data):
   topicKey = __opts__.get("grid.key", "Not Set")
   topicEndpoint = __opts__.get("grid.endpoint", "Not Set")
   credential = AzureKeyCredential(topicKey)
   client = EventGridPublisherClient(topicEndpoint, credential)
   event = EventGridEvent(
-      data={"team": "azure-sdk"},
+      data=data,
       subject="presence/minionfromrunner",
       event_type="minion_presence",
       data_version="1.0"
