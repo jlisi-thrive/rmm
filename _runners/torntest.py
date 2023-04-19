2785e03b2ac23f0f28d6f02153819d52a12cf5d9
jlisi-thrive <96087149+jlisi-thrive@users.noreply.github.com>
2023-04-19 16:11:09 -0400

    Update torntest.py

--- _runners/torntest.py
+++ _runners/torntest.py
@@ -1,12 +1,7 @@
 from salt.ext.tornado.httpclient import AsyncHTTPClient
 from salt.ext.tornado import gen
-
+    
 @gen.coroutine
 def torn():
     http_client = AsyncHTTPClient()
     response = yield http_client.fetch("https://jsonplaceholder.typicode.com/posts/1")
-    # In Python versions prior to 3.3, returning a value from
-    # a generator is not allowed and you must use
-    #   raise gen.Return(response.body)
-    # instead.
-    return "Works"
