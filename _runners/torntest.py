132faf6e8c13239c4f0b0293fa51f8d32fa85cee
jlisi-thrive <96087149+jlisi-thrive@users.noreply.github.com>
2023-04-19 16:21:20 -0400

    Update torntest.py

--- _runners/torntest.py
+++ _runners/torntest.py
@@ -1,7 +1,7 @@
-from salt.ext.tornado.httpclient import AsyncHTTPClient
-from salt.ext.tornado import gen
-    
-@gen.coroutine
+import salt.ext.tornado.gen
+ 
+@salt.ext.tornado.gen.coroutine
 def torn():
     http_client = AsyncHTTPClient()
     response = yield http_client.fetch("https://jsonplaceholder.typicode.com/posts/1")
+    return "Maybe"
